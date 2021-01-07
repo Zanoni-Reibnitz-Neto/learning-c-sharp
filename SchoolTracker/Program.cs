83fbbbaeac19865c55ed7eb5b614e2605b1fce39
zanonineto <zanoni.neto@hbsis.com.br>
2021-01-07 10:29:06 -0300

    2.1

--- SchoolTracker/Program.cs
+++ SchoolTracker/Program.cs
@@ -7,18 +7,22 @@ namespace SchoolTracker
     {
         static void Main(string[] args)
         {
-            var studentsNames = new List<string>();
-            var studentsGrades = new List<int>();
+            var students = new List<Student>();
 
             var addStudent = true;
-            
+
             while (addStudent)
             {
-                Console.WriteLine("Student name: ");
-                studentsNames.Add(ConsoleInterface.ReadString());
-                Console.WriteLine("Student grade: ");
-                studentsGrades.Add(ConsoleInterface.ReadInt());
-                
+                var newStudent = new Student();
+
+                Console.WriteLine("Student name: "); newStudent.Name = ConsoleInterface.ReadString();
+                Console.WriteLine("Student grade: "); newStudent.Grade = ConsoleInterface.ReadInt();
+                Console.WriteLine("Student birthday: "); newStudent.Birthday = ConsoleInterface.ReadString();
+                Console.WriteLine("Student address: "); newStudent.Address = ConsoleInterface.ReadString();
+                Console.WriteLine("Student phone: "); newStudent.Phone = ConsoleInterface.ReadInt();
+
+                students.Add(newStudent);
+
                 Console.WriteLine("Add another? y/n");
                 if (ConsoleInterface.ReadString() != "y")
                 {
@@ -26,10 +30,19 @@ namespace SchoolTracker
                 }
             }
 
-            for (int i = 0; i < studentsNames.Count; i++)
+            foreach (var student in students)
             {
-                Console.WriteLine("Name: {0}, Grade: {1}", studentsNames[i], studentsGrades[i]);
+                Console.WriteLine("Name: {0}, Grade: {1}", student.Name, student.Grade);
             }
         }
+    }
+
+    class Student
+    {
+        public string Name { get; set; }
+        public int Grade { get; set; }
+        public string Birthday { get; set; }
+        public string Address { get; set; }
+        public int Phone { set; get; }
     }
 }
