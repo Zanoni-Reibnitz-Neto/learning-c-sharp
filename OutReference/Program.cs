fcb8ed3bb134187208d55cddf3f846522b0ea721
zanonineto <zanoni.neto@hbsis.com.br>
2021-01-07 22:57:54 -0300

    4.5

---
+++ OutReference/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace OutReference
+{
+    internal static class Program
+    {
+        static void Main(string[] args)
+        {
+            var x = 2;
+            var y = 2;
+            Console.WriteLine(x);
+            Five(out x);
+            Console.WriteLine(x);
+            Console.WriteLine(y);
+            Double(ref y);
+            Console.WriteLine(y);
+        }
+
+        private static void Five(out int a)
+        {
+            a = 5;
+            Console.WriteLine(a);
+        }
+
+        private static void Double(ref int a)
+        {
+            a = 2 * a;
+            Console.WriteLine(a);
+        }
+    }
+}
