4d73b16093c13970c2e74153151a5a0c2da5da81
zanonineto <zanoni.neto@hbsis.com.br>
2021-01-07 23:36:20 -0300

    4.9

---
+++ GeometryTool/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace GeometryTool
+{
+    internal static class Program
+    {
+        private static void Main(string[] args)
+        {
+            var square = new Square() {Width = 2};
+            var triangle = new Triangle() {Base = 2, Height = 5};
+            square.Display();
+            triangle.Display();
+        }
+    }
+
+    internal abstract class Shape
+    {
+        protected abstract int GetArea();
+
+        public void Display()
+        {
+            Console.WriteLine("The area is {0}", GetArea());
+        }
+    }
+
+    internal class Square : Shape
+    {
+        public int Width;
+
+        protected override int GetArea()
+        {
+            return Width * Width;
+        }
+    }
+
+    internal class Triangle : Shape
+    {
+        public int Base;
+        public int Height;
+
+        protected override int GetArea()
+        {
+            return (Base * Height) / 2;
+        }
+    }
+}
